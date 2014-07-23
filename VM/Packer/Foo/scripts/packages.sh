122ebdb1879fe58cb7643f84183c3925d35b35f2
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2014-07-23 16:07:33 +0200

    Cont'd.

---
+++ VM/Packer/Foo/scripts/packages.sh
@@ -0,0 +1,12 @@
+#sudo apt-get update
+sudo apt-get install -y git \
+                        curl \
+                        vim \
+                        htop \
+                        bzip2 \
+                        gcc \
+                        make \
+                        linux-headers-$(uname -r) \
+                        php5 \
+                        php5-cli \
+                        php5-fpm
