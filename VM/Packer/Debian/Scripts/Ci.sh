c1904cc90ec37448fb7075d21e82e0726204ec01
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2014-07-24 16:38:41 +0200

    Bar.

--- VM/Packer/Debian/Scripts/Ci.sh
+++ VM/Packer/Debian/Scripts/Ci.sh
@@ -4,5 +4,5 @@ sudo sed -i'' -r 's/;?listen = (.*)$/listen = 9001/' \
 sudo git clone http://git.hoa-project.net/Central.git /usr/local/lib/Hoa.central
 sudo ln -s /usr/local/lib/Hoa.central/Hoa /usr/local/lib/Hoa
 
-sudo git clone http://github.com/Hywan/CI.git /usr/local/lib/Ci
+sudo git clone https://github.com/Hywan/CI.git /usr/local/lib/Ci
 sudo ln -s /usr/local/lib/Ci/Standby/Application /Ci
