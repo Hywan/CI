a3baa9fa056613cbddd429d1b707823a055eec71
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2014-07-31 11:22:16 +0200

    Install different versions of PHP.

--- VM/Packer/Debian/Scripts/Ci.sh
+++ VM/Packer/Debian/Scripts/Ci.sh
@@ -1,6 +1,10 @@
+#!/bin/bash
+
+# FPM
 sudo sed -i'' -r 's/;?listen = (.*)$/listen = 9001/' \
     /etc/php5/fpm/pool.d/www.conf
 
+# Hoa
 sudo git clone \
     http://git.hoa-project.net/Central.git \
     /usr/local/lib/Hoa.central
@@ -11,6 +15,7 @@ sudo ln -s \
     /usr/local/lib/Hoa/Core/Bin/hoa \
     /usr/local/bin/hoa
 
+# CI
 sudo git clone \
     https://github.com/Hywan/CI.git \
     /usr/local/lib/Ci
@@ -18,6 +23,7 @@ sudo ln -s \
     /usr/local/lib/Ci/Standby/Application \
     /Ci
 
+# atoum
 sudo git clone \
     --branch edge \
     --single-branch \
@@ -27,9 +33,43 @@ sudo ln -s \
     /usr/local/lib/atoum/bin/atoum \
     /usr/local/bin/atoum
 
+# Composer
 curl -sS \
     https://getcomposer.org/installer | \
     sudo php -- --install-dir /usr/local/lib
 sudo ln -s \
     /usr/local/lib/composer.phar \
     /usr/local/bin/composer
+
+# PHP
+sudo mkdir -p /Development/Php
+sudo chown -R packer:packer /Development
+
+cd /Development/Php
+
+git clone \
+    https://git.php.net/repository/php-src.git \
+    Source
+
+cd Source
+
+for version in `echo '5.5.3 5.5.9'`; do
+    mkdir /Development/Php/$version
+    git checkout 'PHP-'$version
+    ./buildconf --force
+    ./configure \
+        --prefix=/Development/Php/$version/ \
+        --disable-all \
+        --enable-fpm
+    make
+    make install
+    make clean
+    make distclean
+
+    mv /Development/Php/$version/etc/php-fpm.conf.default \
+       /Development/Php/$version/etc/php-fpm.conf
+    sed -i'' -r 's/;?listen = (.*)$/listen = 9001/' \
+        /Development/Php/$version/etc/php-fpm.conf
+
+    echo 'PHP-'$version >> /Development/Php/Pool
+done
