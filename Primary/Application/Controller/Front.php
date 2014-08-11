d1fec0466941c16c739467b0df7fcb90bf4a9dc4
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2014-08-11 20:12:24 +0200

    Add a working test.

--- Primary/Application/Controller/Front.php
+++ Primary/Application/Controller/Front.php
@@ -5,6 +5,7 @@ namespace Application\Controller;
 use Hoa\Core;
 use Hoa\Database;
 use Hoa\Socket;
+use Hoa\Http;
 use Hoa\Websocket;
 use Application\Model\Job;
 
@@ -61,5 +62,46 @@ class Front extends Generic {
         $this->data->job = $data;
         $this->view->addOverlay('hoa://Application/View/En/Job.xyl');
         $this->render();
+    }
+
+    public function TestAction ( ) {
+
+        require_once 'hoa://Application/Database.php';
+        $database  = Database\Dal::getInstance('authorizations');
+        $statement = $database->prepare(
+            'SELECT token FROM oauth_tokens ' .
+            'WHERE resource_owner = :resource_owner'
+        );
+        $result    = $statement->execute(['resource_owner' => 'github'])
+                               ->fetchAll()[0];
+
+        print_r($result);
+
+        $body = json_encode([
+            'state'       => 'success',
+            'target_url'  => 'http://127.0.0.1/job/123',
+            'description' => 'Hoa\'s CI: bla bla',
+            'context'     => 'That\'s a context.'
+        ]);
+        $request = new Http\Request();
+        $request->setMethod($request::METHOD_POST);
+        $request->setUrl('/repos/Hywan/Foobar/statuses/f9be0e0472e3df3997052c799f9e01cbf8a85f92');
+        $request['Host']           = 'api.github.com';
+        $request['User-Agent']     = 'hoa/http';
+        $request['Connection']     = 'close';
+        $request['Accept']         = '*/*';
+        $request['Content-Type']   = 'application/json';
+        $request['Content-Length'] = strlen($body);
+        $request['Authorization']  = 'token ' . $result['token'];
+        $request->setBody($body);
+
+        echo $request . $request->getBody();
+
+        $client = new Socket\Client('tcp://api.github.com:443');
+        $client->connect();
+        $client->setEncryption(true, $client::ENCRYPTION_TLS);
+        $client->writeAll($request . $request->getBody() . CRLF);
+
+        var_dump($client->readAll());
     }
 }
