0307640425799aed6c1adcb74c9a0daa5acfb038
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2014-08-12 16:46:57 +0200

    Create user and group.

--- VM/Packer/Debian/Scripts/Initialize.sh
+++ VM/Packer/Debian/Scripts/Initialize.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-# Prepare the next provisionner.
+# Directory to exchange files during the provisionning
 sudo mkdir -p /tmp/Packer
+
+# Users and groups
+sudo groupadd ci
+sudo useradd \
+    --no-create-home
+    --shell /bin/bash
+    --gid ci
+    ci
